eafe3622a15939ef4da4c2fd3718eb11a11898cd
bobUA <31321469+bobUA@users.noreply.github.com>
2019-08-13 08:09:17 -0400

    update

--- simulate_M3RescorlaWagner_v1.m
+++ simulate_M3RescorlaWagner_v1.m
@@ -1,9 +1,12 @@
 
-function [a, r] = simulate_M3RescorlaWagner_v1(T, mu, alpha, beta)
+function [a, r, Q_stored] = simulate_M3RescorlaWagner_v1(T, mu, alpha, beta)
 
 Q = [0.5 0.5];
 
 for t = 1:T
+    
+    % store values
+    Q_stored(:,t) = Q;
     
     % compute choice probabilities
     p = exp(beta*Q) / sum(exp(beta*Q));
